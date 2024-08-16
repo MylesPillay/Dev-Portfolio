24176ebed0a96bf9ee99fe565d5a5a1d9379abff
4naans <mylespillay@yahoo.co.uk>
2024-08-16 22:55:57 +0100

    nav bar work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,82 @@
+"use client";
 import { Metadata } from "next";
 import Link from "next/link";
 import { Inter } from "next/font/google";
 import "./globals.css";
-const inter = Inter({ subsets: ["latin"] });
+import { FaUser, FaEnvelope, FaMicrophone, FaLaptopCode } from "react-icons/fa";
+import { useState } from "react";
 
-export const metadata: Metadata = {
-	title: "Myles Pillay - Portfolio",
-	description: "Software Developer Portfolio"
-};
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
 	children
 }: {
 	children: React.ReactNode;
 }) {
+	const [hovered, setHovered] = useState<string | null>(null);
+
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
 				<div className='flex h-screen'>
-					<nav className='flex flex-col h-full w-[10%] bg-yellow-500 text-black font-bold text-justify'>
-						<div className='flex items-center justify-center h-20'>
-							<h1 className='text-xl text-center'>
-								Myles Pillay
-							</h1>
-						</div>
-						<ul className='flex flex-row h-full'>
-							<li className='h-full flex flex-col justify-end pb-4'>
-								<Link
-									href='/'
-									className='text-3xl tracking-widest text-left writing-vertical'>
-									About Me
-								</Link>
-							</li>
-							<li className='h-full flex flex-col justify-end pb-4'>
-								<Link
-									href='/projects'
-									className='text-3xl tracking-widest text-left writing-vertical'>
-									Projects
-								</Link>
-							</li>
-							<li className='h-full flex flex-col justify-end pb-4'>
-								<Link
-									href='/interests'
-									className='text-3xl tracking-widest text-left writing-vertical'>
-									Interests
-								</Link>
-							</li>
-							<li className='h-full flex flex-col justify-end pb-4'>
-								<Link
-									href='/contact'
-									className='text-3xl tracking-widest text-left writing-vertical'>
-									Contact
-								</Link>
-							</li>
+					<nav className='flex flex-col h-full w-auto min-w-14 p-4 bg-yellow-500 text-black font-bold text-justify'>
+						<ul className='flex flex-row h-full w-full space-x-1'>
+							{[
+								{
+									href: "/",
+									label: "About Me",
+									icon: <FaUser />
+								},
+								{
+									href: "/projects",
+									label: "Projects",
+									icon: <FaLaptopCode />
+								},
+								{
+									href: "/interests",
+									label: "Interests",
+									icon: <FaMicrophone />
+								},
+								{
+									href: "/contact",
+									label: "Contact",
+									icon: <FaEnvelope />
+								}
+							].map((item) => (
+								<li
+									key={item.href}
+									className='h-full flex flex-col justify-start '
+									onMouseEnter={() => setHovered(item.href)}
+									onMouseLeave={() => setHovered(null)}>
+									<Link
+										href={item.href}
+										className='flex items-center justify-center text-3xl tracking-widest text-left writing-vertical min-width-[25%]'>
+										{hovered === item.href ? (
+											<span className='animate-bounce-in-top'>
+												{item.label
+													.toUpperCase()
+													.split("")
+													.reverse()
+													.map((letter, index) => (
+														<span
+															key={index}
+															className={`inline-block rotate-90 ${
+																letter === " "
+																	? "py-2"
+																	: "py-1"
+															}`}>
+															{letter}
+														</span>
+													))}
+											</span>
+										) : (
+											<span className='px-[3px]'>
+												{item.icon}
+											</span>
+										)}
+									</Link>
+								</li>
+							))}
 						</ul>
 					</nav>
 					<main className='flex-grow overflow-y-auto p-8'>
