7428f7484ecfcca3a683a2620669d4b556249aed
4naans <mylespillay@yahoo.co.uk>
2024-09-09 11:54:37 +0100

    image updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,43 @@ import AboutMeHeader from "./components/aboutme/AboutMeHeader";
 import MobileAboutMeHeroSection from "./components/aboutme/MobileAboutMeHeroSection";
 import MobileTechSkillsSection from "./components/aboutme/MobileSkillsSections";
 import MobileSoftSkillsSection from "./components/aboutme/MobileSoftSkillsSection";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+	process.env.SUPABASE_API_URL as string,
+	process.env.SUPABASE_API_SECRET_ACCESS_TOKEN as string
+);
 
 export default function Home() {
 	const [singularExpansion, setSingularExpansion] = useState("none");
+	const [loading, setLoading] = useState<boolean>(false);
+	const [image, setImage] = useState<string>("");
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const fetchImage = async () => {
+			try {
+				setLoading(true);
+
+				const image = await supabase.storage
+					.from("portfolio_images")
+					.getPublicUrl(`profile/profile.jpg`).data.publicUrl;
+				if (image) {
+					setImage(image);
+					setLoading(false);
+				}
+			} catch (error) {
+				setError("Failed to fetch image");
+				console.error(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchImage();
+	}, [image, loading, setLoading]);
+
 	return (
 		<div className='bg-projects-gradient lg:h-screen h-screen w-full overflow-x-hidden lg:overflow-y-scroll  overflow-y-scroll  border-0 sm:border-l border-orangeflame '>
 			<div className='flex flex-col justify-between w-full'>
@@ -17,10 +50,16 @@ export default function Home() {
 					<div className='flex flex-col  w-full justify-start align-middle items-start '>
 						<div className='flex flex-col w-full h-auto rounded-lg items-start justify-start'>
 							<div className='hidden md:flex w-[100%]'>
-								<AboutMeHeroSection />
+								<AboutMeHeroSection
+									image={image}
+									loading={loading}
+								/>
 							</div>
 							<div className='md:hidden flex w-[100%]'>
-								<MobileAboutMeHeroSection />
+								<MobileAboutMeHeroSection
+									image={image}
+									loading={loading}
+								/>
 							</div>
 							{/* <div className='lg:hidden flex w-[100%]'>
 								<MobileAboutMeHeroSection />
