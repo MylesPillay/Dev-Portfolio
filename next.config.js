0f5739060558011a1ca51408e6cf8dc60b8b1fdc
4naans <mylespillay@yahoo.co.uk>
2026-03-07 21:50:35 +0000

    updated supabase project id

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	env: {
-		SUPABASE_API_URL: "https://mgbwyyztxdknsphcbtof.supabase.co",
+		SUPABASE_API_URL: "https://hzmlorqgafiemtplcacd.supabase.co",
 		SUPABASE_API_SECRET_ACCESS_TOKEN:
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nYnd5eXp0eGRrbnNwaGNidG9mIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTU1MzAxMywiZXhwIjoyMDQxMTI5MDEzfQ.wo4aqOWfpGLlNWO9_KgFrLTW5nUHwIDux0AG9TV1TdI"
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6bWxvcnFnYWZpZW10cGxjYWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTI0NTIsImV4cCI6MjA4ODQ4ODQ1Mn0.viA63XO8eTa29VrjhTkh-d8vRuEbDubYgLPzz6GFYLs"
 	}
 };
