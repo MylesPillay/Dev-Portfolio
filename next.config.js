3a66d34189c863988868de59644754177af0d9e8
4naans <mylespillay@yahoo.co.uk>
2026-03-25 17:11:21 +0000

    removed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-	env: {
-		SUPABASE_API_URL: "https://hzmlorqgafiemtplcacd.supabase.co",
-		SUPABASE_API_SECRET_ACCESS_TOKEN:
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6bWxvcnFnYWZpZW10cGxjYWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MTI0NTIsImV4cCI6MjA4ODQ4ODQ1Mn0.viA63XO8eTa29VrjhTkh-d8vRuEbDubYgLPzz6GFYLs"
-	}
-};
+module.exports = {};
